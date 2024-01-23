8ae7275cc95674311b696c4c006f3b820abae588
Ruchitk620 <157483500+Ruchitk620@users.noreply.github.com>
2024-01-23 17:16:41 +0530

    Update ChildInterface.java

--- interfaces/extendinginterfaces/ChildInterface.java
+++ interfaces/extendinginterfaces/ChildInterface.java
@@ -1,5 +1,5 @@
 //child interface inherits from InterfaceOne//
-package com.tnsif.interfaces.extendinginterfaces;
+package interfaces.extendinginterfaces;
 
 public interface ChildInterface extends InterfaceOne {
 	void show();
